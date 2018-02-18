f81bdc9990f62656e46aaecfeec900fa82e5fb5b
Nelly Sugu <nelly.sugu@gmail.com>
2018-02-17 22:22:11 -0800

    Gets current, previous,and next dates doodles with google brand colors

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# today-in-google-doodles-history
-Looking back in google doodles history which dates back from August 30 1998 show doodles for this day
+## Today in Google Doodles history
+Go back in Google Doodles history and get doodles for any date
+According to [wikipedia](https://en.wikipedia.org/wiki/Google_Doodle), Google Doodles history starts on 1998/8/30 when Larry Page and Sergey Brin designed the first Google Doodle to let Google users at the time that Larry and Sergey were at the Burning Man Festival, just in case the servers crashed :).
 
-To get it to work in Chrome, Add [this extension](https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en) to Chrome and enable it.
-It has to do with CORS.
+Ever since, Google has been designing Google Doodles to celebrate different people and events through out history. I've always been intrigued by Google Doodles. Apparently there is a whole team at Google that makes these things. The coolest thing about them is the [Doogle 4 Google Challenge](https://doodles.google.com/d4g/) which allows any one to submit a Google doodle for a prize and their art can be used worldwide!
+
+Anyway, I am no artist, so here is my Google Doodle contribution!
+
+#Note: To get it to work in Chrome, Add [the Allow-Control-Allow-Origin: * extension](https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en) to Chrome and enable it. Otherwise you will get a `No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.` error.
--- scripts.js
+++ scripts.js
@@ -1,25 +1,194 @@
 $(document).ready(function(){
-	// Today in Google doodles
+	// Today in Google doodles history
+	// Go back in Google doodles history and get doodles for this date
+	// Google doodles history starts on 1998/8/30
+	// https://en.wikipedia.org/wiki/Google_Doodle
 
 	// Parse current date for year, month, date
-	let currentDate = getFormattedDate(new Date("2017/1/28"));
+	let currentDate = getFormattedDate(new Date());
+
+	// Change background colors on hover
+	$("body").on('mouseenter', '.btn-primary, button', function(){
+		let bgColor = getRandGoogleColor(); // + '!important';
+		$(this).css('background-color', bgColor);
+	});
+	
+	$("body").on('mouseleave', '.btn-primary, button', function(){
+		let bgColor = getRandGoogleColor(); // + ' !important';
+		$(this).css('background-color', bgColor);
+	});
+
+	$('body').on('click', '#prev button', function(event){
+		event.preventDefault();
+
+		currentDate = getConsecutiveDate(currentDate, 'prev');
+		renderDoodles(currentDate);
+	})
+
+	$('body').on('click', '#next button', function(event){
+		event.preventDefault();
+
+		currentDate = getConsecutiveDate(currentDate, 'next');
+		renderDoodles(currentDate);
+	})
+
 	
 
-	// Go back in Google doodles history and get today's doodles
-	// start: 1998/8/30
-	// https://en.wikipedia.org/wiki/Google_Doodle
-	getDoodlesByDay(currentDate);
-	
+	renderPageTitle();
+	renderDoodles(currentDate);
 
-	function getDoodlesByDay(date){
+	
+	/************
+	UI Helpers
+	************/
+	function renderDoodles(date){
+		renderPageSubTitle(date);
+
+		$('.container').empty();
+		getTodaysDoodlesInHistory(date);
+	}
+
+	function showDoodles(doodles){
+		let doodlesCount = 0;
+		let container = $('.container');
+
+		container.empty();
+
+		let row = ""; // Variable to hold our row
+
+		doodles.sort(sortDoodlesByDate)
+
+		doodles.forEach(function(doodle){
+			let card = $(createDoodleCard(doodle));
+
+			// Show 3 doodles per row if we can
+			if (doodlesCount <= 3){
+				
+				if (doodlesCount === 0){
+					// Create a new row
+					row = $('<div>').addClass('row');
+				}
+
+				var col = $('<div>').addClass('col-xs-12 col-sm-6 col-md-4');
+				col.html(card);
+				row.append(col);
+				doodlesCount++;
+			}
+
+			if (doodlesCount === 3){
+				doodlesCount = 0;
+				container.append(row);
+			}
+		});
+
+		container.append(row);
+	}
+
+	function createDoodleCard(doodle){
+		let card = '<div class="card">' +
+		  				'<img class="card-img-top" src="' + doodle.url + '" alt="Card image cap">' +
+		  				'<div class="card-body">' + 
+					    '<h5 class="card-title">' + doodle.title + '</h5>' +
+					    '<p class="card-text">' + doodle.sub_title + '</p>' +
+					    '<a href="' + doodle.google_query + '"target="_blank" class="btn btn-primary ' + getRandGoogleColorClass() + '">' + 'What in the doodle?' + '</a>' +
+					  '</div>' +
+					'</div>';
+		return card;
+	}
+
+	function getRandGoogleColorClass(){
+		// Give our buttons random google colors
+		let google_colors = ['google-blue', 'google-green', 'google-yellow', 'google-red'];
+		let randColorIdx = Math.floor(Math.random() * google_colors.length);
+
+		return google_colors[randColorIdx]
+	}
+
+	function getRandGoogleColor(){
+		// Give our buttons random google colors
+		let google_colors = ['#4285f4', '#34a853', '#fbbc05', '#ea4335'];
+		let randColorIdx = Math.floor(Math.random() * google_colors.length);
+
+		return google_colors[randColorIdx]
+	}
+
+	function parseImgUrl(url){
+		// We get some urls in a 'https://url' and others in a '//url' format
+		// Build urls ourselves uniformly to avoid confusion
+		let urlParts = url.split('//');
+
+		return 'https://' + urlParts[1];
+	}
+
+	function renderPageTitle(){
+		// Color each letter of our header with a rondom Google brand color
+		let header = 'Today in Google doodles history';
+		let headerLetters = header.split('');
+		let newHeader = '';
+		headerLetters.forEach(function(letter){
+			newHeader += '<span class="' + getRandGoogleColorClass() + '">' + letter + '</span>';
+		})
+
+		$('#title').html(newHeader);
+	}
+
+	function renderPageSubTitle(date){
+		// Populate sub-title with current date
+		$('#current-date').text(getFormattedDateString(date));
+
+		// Give prev and next buttons random Google brand colors
+		$('#prev button').addClass(getRandGoogleColorClass());
+		$('#next button').addClass(getRandGoogleColorClass());
+	}
+
+	/************
+	Date Helpers
+	*************/
+
+	function getTodaysDoodlesInHistory(date){
+		// Google doodles history starts in 1998
+		let todaysDoodles = [];
+		for (let year=date.year; year >= 1998; year--){
+			getDoodlesByDate(todaysDoodles, year, date.monthAsNumber, date.day);
+		}
+	}
+
+	function getDoodlesByDate(todaysDoodles, year, month, day){
 		$.ajax({
-			url: 'https://www.google.com/doodles/json/' + date.year + '/' + date.monthAsNumber + '?hl=en',
+			url: 'https://www.google.com/doodles/json/' + year + '/' + month + '?hl=en',
 			method: 'GET'
 		}).then(function(doodles){
-			let todayDoodles = filterDoodlesByDay(doodles, date.day);
-			showDoodles(todayDoodles);
+			let filteredDoodles = filterDoodlesByDay(doodles, day);
+			
+			filteredDoodles.forEach(function(doodle){
+				let doodleDate = getFormattedDate(getDoodleDate(doodle));
+
+				let newDoodle = {
+					id: Number(getDoodleDate(doodle)),
+					date:{
+						year: doodleDate.year,
+						month: doodleDate.monthAsNumber,
+						day: doodleDate.day
+					},
+					url: parseImgUrl(doodle.hires_url),
+					title: doodle.query,
+					sub_title: getFormattedDateString(doodleDate),
+					google_query: 'https://www.google.com/search?q=' + doodle.query + ' ' + doodleDate.day + ' ' + doodleDate.monthAsNumber + ' ' + doodleDate.year
+				}
+
+				todaysDoodles.push(newDoodle);
+			})
+
+			if (filteredDoodles.length){
+				showDoodles(todaysDoodles);
+			}
+			
 			
 		});
+	}
+
+	function getFormattedDateString(doodleDate){
+		return doodleDate.monthAsString + ' ' + doodleDate.day + ', ' + doodleDate.year
 	}
 
 	function filterDoodlesByDay(doodles, day){
@@ -31,65 +200,19 @@ $(document).ready(function(){
 		})
 	}
 
-	
-	/************
-	UI Helpers
-	************/
-	function showDoodles(doodles){
-		let doodlesCount = 0;
-		let container = $('.container');
-
-		let row = ""; // Variable to hold our row
-
-		doodles.forEach(function(doodle){
-			let card = $(createDoodleCard(doodle));
-
-			if (doodlesCount === 0){
-				// Create a new row
-				row = $('<div>').addClass('row');
-			}
-
-			// Show 4 doodles per row if we can
-			if (doodlesCount < 4){
-				var col = $('<div>').addClass('col-xs-12 col-sm-6 col-md-4');
-				col.html(card);
-				row.append(col)
-				doodlesCount++;
-			}else{
-				doodlesCount = 0;
-			}
-			
-			
-		});
-
-		container.append(row);
+	function sortDoodlesByDate(doodle1, doodle2){
+		return doodle2.id - doodle1.id;
 	}
 
-	function createDoodleCard(doodle){
-		let doodleDate = getDoodleDate(doodle);
-
-		let googleQuery = 'https://www.google.com/search?q=' + doodle.query + ' ' + doodleDate.day + ' ' + doodleDate.monthAsNumber + ' ' + doodleDate.year;
-
-		let doodleSubTitle = '' + doodleDate.monthAsString + ' '
-
-		// let verb = isPast(doodleDate, getFormattedDate(new Date()))? 'Was' : 'Is';
-
-		let cardImgSrc = 'http://' + doodle.hires_url;
-
-		let card = '<div class="card">' +
-		  				'<img class="card-img-top" src="' + cardImgSrc + '" alt="Card image cap">' +
-		  				'<div class="card-body">' + 
-					    '<h5 class="card-title">' + doodle.query + '</h5>' +
-					    '<p class="card-text">' + doodle.title + '</p>' +
-					    '<a href="' + googleQuery + '"target="_blank" class="btn btn-primary">' + 'What in the doodle?' + '</a>' +
-					  '</div>' +
-					'</div>';
-		return card;
+	function generateDoodleYearsUntil(date){
+		// Doodles years starts in 1998
+		let doodleYears = [];
+		for (let year=date.year; year >= 1998; year--){
+			doodleYears.push(year);
+		}
+		return doodleYears;
 	}
 
-	/************
-	Date Helpers
-	*************/
 	function getFormattedDate(date){
 		return {
 			year: date.getFullYear(),
@@ -105,7 +228,7 @@ $(document).ready(function(){
 		let doodleDateString = doodle.run_date_array.reduce(function(acc, next){
 			return acc += next + '/'
 		}, '');
-		return getFormattedDate(new Date(doodleDateString));
+		return new Date(doodleDateString);
 	}
 
 	function isPast(date1, date2){
@@ -114,6 +237,18 @@ $(document).ready(function(){
 		}else{
 			return false;
 		}
+	}
+
+	function getConsecutiveDate(doodleDate, type){
+		let tempCurrentDate = new Date(getFormattedDateString(doodleDate));
+
+		if (type === 'next'){
+			tempCurrentDate = tempCurrentDate.setDate(tempCurrentDate.getDate() + 1);
+		}else if (type == 'prev'){
+			tempCurrentDate = tempCurrentDate.setDate(tempCurrentDate.getDate() - 1);
+		}else{}
+		
+		return getFormattedDate(new Date(tempCurrentDate));
 	}
 
 })
