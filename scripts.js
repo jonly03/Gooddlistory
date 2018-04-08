cad11c399c897990ab91aff44dab41034501db65
Joseph Nelly Sugu Sugira <jonly03@users.noreply.github.com>
2018-04-07 18:54:02 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 		renderDoodles(currentDate);
 	})
 
-	$('body').on('click', '#current-date', function(evernt){
+	$('body').on('mouseenter', '#current-date', function(evernt){
 		$('#static-date').addClass('hide');
 		$('#edit-date').removeClass('hide');
 		$('#submit').css('background-color', getRandGoogleColor());
